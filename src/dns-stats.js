6cdd1cfcc1a482d361c3bd673f742fd0133d1a24
Andrew <andrei.sp.93@gmail.com>
2025-01-06 01:10:24 +0300

    feat: finish dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const totalDomains = domains.length;
+  const dnsStats = {}; 
+  const domainParts = [];
+  domains.forEach((domain) => {
+    let keyPart = "";
+    for (let i = 0; i < domain.length; i++) {
+      const char = domain[i];
+      if (char !== ".") {
+        keyPart += char;
+      } else {
+        domainParts.push(keyPart);
+        keyPart = "";
+      }
+    }
+    if (keyPart) {
+      domainParts.push(keyPart);
+    }
+  });
+  const reversedParts = domainParts.reverse();
+  const uniqueParts = Array.from(new Set(reversedParts));
+  let dnsKey = "";
+  uniqueParts.forEach((part, index) => {
+    dnsKey += "." + part;
+    console.log(dnsKey);
+    if (index >= 0 && index <= 1) {
+      dnsStats[dnsKey] = totalDomains;
+    } else {
+      dnsStats[dnsKey] = 1;
+    }
+  });
+
+  return dnsStats;
 }
 
 module.exports = {
