6b34c508aae056c61e5cab9e86bf17e2ea7d1241
Andrew <andrei.sp.93@gmail.com>
2025-01-06 22:01:22 +0300

    fix: fix the last task

--- src/what-season.js
+++ src/what-season.js
@@ -13,46 +13,33 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 function getSeason(date) {
   if (!date) {
-    return 'Unable to determine the time of year!';
+    return "Unable to determine the time of year!";
   }
-
-  // Проверка на тип даты и корректность
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
-    throw new Error('Invalid date!');
+  if (date[Symbol.toStringTag] === "Date") {
+    throw new Error("Invalid date!");
   }
-
-  // Проверка на "трудные моменты" — объект, который выглядит как дата
-  if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.getTime())) {
-    throw new Error('Invalid date!');
+  if (
+    !(date instanceof Date) ||
+    Object.prototype.toString.call(date) !== "[object Date]" ||
+    isNaN(date.getTime())
+  ) {
+    throw new Error("Invalid date!");
   }
-
-  // Проверка на "очень трудные моменты"
-  try {
-    date.getUTCFullYear();
-  } catch (e) {
-    throw new Error('Invalid date!');
-  }
-
   const month = date.getMonth();
-
-  // Месяцы для сезонов
-  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+  const seasons = ["winter", "spring", "summer", "autumn"];
   const seasonMapping = [
-    [11, 0, 1], // winter
-    [2, 3, 4],  // spring
-    [5, 6, 7],  // summer
-    [8, 9, 10], // autumn
+    [11, 0, 1],
+    [2, 3, 4],
+    [5, 6, 7],
+    [8, 9, 10],
   ];
-
   for (let i = 0; i < seasonMapping.length; i++) {
     if (seasonMapping[i].includes(month)) {
       return seasons[i];
     }
   }
-  throw new Error('Invalid date!');
+  return "Unable to determine the time of year!";
 }
-
-
 
 module.exports = {
   getSeason,
